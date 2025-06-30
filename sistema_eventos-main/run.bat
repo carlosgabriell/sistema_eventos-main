3021ea55a93d64f4f8a89837772bf7c21a8df294
Carlos Gabriel <83463297+carlosgabriell@users.noreply.github.com>
2025-06-30 18:07:54 -0300

    Delete sistema_eventos-main/run.bat

--- sistema_eventos-main/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_FX_LIB="C:\Program Files\Java\javafx-sdk-21.0.7\lib"
-
-echo === Executando app.jar...
-java --module-path %JAVA_FX_LIB% --add-modules javafx.controls,javafx.fxml -jar app.jar
-pause
